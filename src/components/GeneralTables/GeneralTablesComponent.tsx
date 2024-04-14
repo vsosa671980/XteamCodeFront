05a446f80dcdab3e571ed469d194db23bff111bf
Vicente Sosa <vsosaal@gmail.com>
2024-04-14 21:09:17 +0200

    commit Save

---
+++ src/components/GeneralTables/GeneralTablesComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from "./GeneralTables.module.css";
+
+export default function GeneralTablesComponent() {
+  return (
+    <div>
+
+        <table>
+            <thead>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+      
+    </div>
+  )
+}
